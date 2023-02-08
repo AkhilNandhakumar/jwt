425f063fc051ffaa2423603e5b8f1b327a5a8db8
akhil <akhil@nandhakumar.net>
2023-02-08 12:55:22 -0800

    things

--- src/main/java/com/nighthawk/spring_portfolio/mvc/note/NoteJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/note/NoteJpaRepository.java
@@ -1,11 +1,17 @@
 package com.nighthawk.spring_portfolio.mvc.note;
 
-import com.nighthawk.spring_portfolio.mvc.person.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface NoteJpaRepository extends JpaRepository<Note, Long> {
-    List<Note> findAllByPerson(Person p);
+    List<Person> findByPersonId(Long id);
+
+    @Transactional
+    void deleteByPersonId(long id);
 }
 
